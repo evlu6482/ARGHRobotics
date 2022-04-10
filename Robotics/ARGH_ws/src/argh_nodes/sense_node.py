83c6137405660ef90c0371bdd0e0fa17c3c093dd
ThomasJPSAtom <thomasjpshepard@gmail.com>
2022-04-10 15:53:06 -0600

    Finalize sense_node.py and delete extra files

--- Robotics/ARGH_ws/src/argh_nodes/sense_node.py
+++ Robotics/ARGH_ws/src/argh_nodes/sense_node.py
@@ -11,8 +11,8 @@ pub_sense = rospy.Publisher('sensing_node_input',Bool,queue_size = 10) # reset n
 pub_move = rospy.Publisher('sensing_node_boolean_move',Bool, queue_size= 10)
 pub_geom = rospy.Publisher('coordinate_tomato',Pose, queue_size = 10)
 Camera_Location = 1
-script_location = ""
-coord_location = "/home/argh/Documents/ARGHRobotics/Software/Tomato_MaskRCNN/Mask_Exports"
+script_location = "/home/argh/Documents/ARGHRobotics/Software/Tomato_MaskRCNN/Scripts/Main"
+coord_file = "/home/argh/Documents/ARGHRobotics/Software/Tomato_MaskRCNN/Mask_Exports/Coordinates.csv"
 
 def sense_callback(data):
     rate = rospy.Rate(2)
@@ -21,21 +21,17 @@ def sense_callback(data):
         rospy.loginfo("Beginning Sensing")
 
         # create csv with camera location
-
+        np
         # write Camera_Location to text file
         os.path.join(script_location)
         os.system("conda run -n maskrcnn python -Wignore ARGH_Driver_V1.py")
-        os.path.join(coord_location)
-        np.loadtxt("Coordinates.csv")
-        #
-        # read values from csv
-        tomato_found=False
-        geometry = Pose()
-        geometry.position.x = ax
-        geometry.position.y = ay
-        geometry.position.z = az
-
+        tomato_found = os.path.exists(coord_file)
         if(tomato_found):
+            coords = np.loadtxt(coord_file)
+            geometry = Pose()
+            geometry.position.x = coords[0]
+            geometry.position.y = coords[1]
+            geometry.position.z = coords[2]
             pub_geom.publish(geometry)
             rate.sleep()
         else:
